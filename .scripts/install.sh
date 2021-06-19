e55a7c3b90ebc863dbe89d9398b46963ac01ce2f
Fowlron <pedromfgodinho@gmail.com>
2021-06-19 11:17:08 +0100

    Renamed scripts to remove dot, added install script

---
+++ .scripts/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Clone repository
+alias tdot='/usr/bin/git --git-dir=$HOME/.tdot --work-tree=$HOME'
+git clone --bare https://github.com/Fowlron/TerminalDotfiles.git $HOME/.tdot
+tdot stash
+tdot checkout
+tdot config --local status.showUntrackedFiles no
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+# zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall 
+
+# Set shell to zsh
+chsh -s $(which zsh)
